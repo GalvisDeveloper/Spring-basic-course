d1123d58dcfbcc62d86d2e378d7af5d83d64d6b4
GalvisDeveloper <galvisdeveloper@gmail.com>
2021-03-19 12:09:51 -0500

    Initial commit

---
+++ src/test/java/com/bolsadeideas/springboot/web/app/section2/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.web.app.section2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
